6f4a5d29a187fed049f93412c80e3c9053b1e622
jRetro <pardojeromeimportant@gmail.com>
2024-02-11 22:12:01 +0800

    anonymously-yours-client

---
+++ client/index.js
@@ -0,0 +1,5 @@
+document.getElementById('LoadingScreenLogo').addEventListener("click", () => {
+    document.getElementById('LoadingScreenLogo').style.opacity = '0';
+    document.getElementById('messages').style.display = 'flex';
+    document.getElementById('notes').style.display = 'flex';
+})
