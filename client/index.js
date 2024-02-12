68e8393e5f086c3980ae7d477857d0ac0baf5dad
jRetro <pardojeromeimportant@gmail.com>
2024-02-12 16:39:32 +0800

    post-page-create

--- client/index.js
+++ client/index.js
@@ -1,5 +1,12 @@
 document.getElementById('LoadingScreenLogo').addEventListener("click", () => {
-    document.getElementById('LoadingScreenLogo').style.opacity = '0';
+    document.getElementById('LoadingScreenLogo').style.display = 'none';
     document.getElementById('messages').style.display = 'flex';
     document.getElementById('notes').style.display = 'flex';
 })
+
+function posts(){
+    document.getElementById('notes').style.display = 'none';
+    document.getElementById('messages').style.display = 'none';
+    document.getElementById('background').style.backgroundImage = 'url(/client/img/background-img-post.png)'
+    document.getElementById('post').style.display = 'flex';
+}
