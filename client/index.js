32a6fb4c61d6bca7ff69b1f9847a04fae5d455e8
jRetro <pardojeromeimportant@gmail.com>
2024-02-12 20:29:37 +0800

    post and pages done client side

--- client/index.js
+++ client/index.js
@@ -2,6 +2,7 @@ document.getElementById('LoadingScreenLogo').addEventListener("click", () => {
     document.getElementById('LoadingScreenLogo').style.display = 'none';
     document.getElementById('messages').style.display = 'flex';
     document.getElementById('notes').style.display = 'flex';
+    document.getElementById('page-count').style.display ='flex'
 })
 
 function posts(){
@@ -9,4 +10,12 @@ function posts(){
     document.getElementById('messages').style.display = 'none';
     document.getElementById('background').style.backgroundImage = 'url(/client/img/background-img-post.png)'
     document.getElementById('post').style.display = 'flex';
+    document.getElementById('page-count').style.display ='none'
+}
+function back(){
+    document.getElementById('post').style.display = 'none';
+    document.getElementById('background').style.backgroundImage = 'url(/client/img/backrgound-img.png)'
+    document.getElementById('messages').style.display = 'flex';
+    document.getElementById('notes').style.display = 'flex';
+    document.getElementById('page-count').style.display ='flex'
 }
